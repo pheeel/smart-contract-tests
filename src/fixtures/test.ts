8b920340e93a9738db37fdd32b458d19f22a934a
Philipp Panteleev <panteleevphil@gmail.com>
2023-05-28 18:13:31 +0200

    debug

--- src/fixtures/test.ts
+++ src/fixtures/test.ts
@@ -16,47 +16,58 @@ type TestFixtures = {
   // Context
   context: BrowserContext;
   metamask: typeof metamask;
+  metamaskSetup: boolean;
 
   // Utils
   helpers: Helpers;
 };
 
 const test = baseTest.extend<TestFixtures>({
-  context: async ({}, use) => {
-    // required for synpress
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    global.expect = expect;
-    // download metamask
-    const metamaskPath = await prepareMetamask(
-      process.env.METAMASK_VERSION || '10.25.0',
-    );
-    // prepare browser args
-    const browserArgs = [
-      `--disable-extensions-except=${metamaskPath}`,
-      `--load-extension=${metamaskPath}`,
-      '--remote-debugging-port=9222',
-    ];
-    if (process.env.CI) {
-      browserArgs.push('--disable-gpu');
+  metamaskSetup: true,
+  context: async ({ metamaskSetup }, use) => {
+    if (metamaskSetup) {
+      // required for synpress
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      global.expect = expect;
+      // download metamask
+      const metamaskPath = await prepareMetamask(
+        process.env.METAMASK_VERSION || '10.25.0',
+      );
+      // prepare browser args
+      const browserArgs = [
+        `--disable-extensions-except=${metamaskPath}`,
+        `--load-extension=${metamaskPath}`,
+        '--remote-debugging-port=9222',
+      ];
+      if (process.env.CI) {
+        browserArgs.push('--disable-gpu');
+      }
+      if (process.env.HEADLESS_MODE) {
+        browserArgs.push('--headless=new');
+      }
+      // launch browser
+      const context = await chromium.launchPersistentContext('', {
+        headless: false,
+        args: browserArgs,
+      });
+      // wait for metamask
+      await context.pages()[0].waitForTimeout(3000);
+      // setup metamask
+      await initialSetup(chromium, {
+        secretWordsOrPrivateKey: 'machine plastic wood coin dose put police coast door poverty fatal until',
+        password: '12345678',
+        enableAdvancedSettings: true,
+      });
+      await use(context);
+      await context.close();
+    } else {
+      // use default browser context
+      const context = await chromium.launchPersistentContext('');
+      // delete the blank page
+      await context.pages()[0].close();
+      await use(context);
+      await context.close();
     }
-    if (process.env.HEADLESS_MODE) {
-      browserArgs.push('--headless=new');
-    }
-    // launch browser
-    const context = await chromium.launchPersistentContext('', {
-      headless: false,
-      args: browserArgs,
-    });
-    // wait for metamask
-    await context.pages()[0].waitForTimeout(3000);
-    // setup metamask
-    await initialSetup(chromium, {
-      secretWordsOrPrivateKey: 'machine plastic wood coin dose put police coast door poverty fatal until',
-      password: '12345678',
-      enableAdvancedSettings: true,
-    });
-    await use(context);
-    await context.close();
   },
 
   metamask: async ({}: never, use: (arg0: never) => never) => {
--- src/tests/test.spec.ts
+++ src/tests/test.spec.ts
@@ -1,6 +1,7 @@
 import test, { expect } from 'fixtures';
 
 test.describe.serial('App', () => {
+  test.use({ metamaskSetup: false });
   test('Test CI', async ({ page }) => {
     await page.goto('https://google.com', { waitUntil: 'domcontentloaded' });
     console.log(page.url());
