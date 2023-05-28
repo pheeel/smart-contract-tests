7c5a240470f29a45357eec0d7655043878374a77
Philipp Panteleev <panteleevphil@gmail.com>
2023-05-28 18:18:32 +0200

    debug

--- src/tests/test.spec.ts
+++ src/tests/test.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from 'fixtures';
 
 test.describe.serial('App', () => {
-  test.use({ metamaskSetup: false });
+  test.use({ metamaskSetup: true });
   test('Test CI', async ({ page }) => {
     await page.goto('https://google.com', { waitUntil: 'domcontentloaded' });
     console.log(page.url());
