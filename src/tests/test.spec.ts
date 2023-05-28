9481bc6d91f2014d8ea1569f047dfde1caa0122d
Philipp Panteleev <panteleevphil@gmail.com>
2023-05-28 17:57:00 +0200

    debug

--- src/tests/test.spec.ts
+++ src/tests/test.spec.ts
@@ -1,7 +1,8 @@
 import test, { expect } from 'fixtures';
 
 test.describe.serial('App', () => {
-  test('Test CI', async () => {
-    await expect(2 + 2).toEqual(5);
+  test('Test CI', async ({ page }) => {
+    console.log(page.url());
+    await expect(2 + 2).toEqual(4);
   });
 });
